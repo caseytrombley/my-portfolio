7a9ac6d1df2a468108a7a3789127dab1581cc349
Casey Trombley <casey.trombley@libertymutual.com>
2018-12-13 13:42:33 -0500

    added to the portfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,7 +5,49 @@ class Portfolio extends Component {
         return (
             <div>
 
-                Portfolio
+                <div className="row">
+                    <div className="col-sm-6 col-md-4">
+                        <div className="thumbnail">
+                            <img src="..." alt="..."/>
+                                <div className="caption">
+                                    <h3>Thumbnail label</h3>
+                                    <p>...</p>
+                                    <p><a href="/" className="btn btn-primary" role="button">Button</a> <a href="/"
+                                                                                                           className="btn btn-default"
+                                                                                                           role="button">Button</a>
+                                    </p>
+                                </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="thumbnail">
+                            <img src="..." alt="..."/>
+                            <div className="caption">
+                                <h3>Thumbnail label</h3>
+                                <p>...</p>
+                                <p><a href="/" className="btn btn-primary" role="button">Button</a> <a href="/"
+                                                                                                       className="btn btn-default"
+                                                                                                       role="button">Button</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="thumbnail">
+                            <img src="..." alt="..."/>
+                            <div className="caption">
+                                <h3>Thumbnail label</h3>
+                                <p>...</p>
+                                <p><a href="/" className="btn btn-primary" role="button">Button</a> <a href="/"
+                                                                                                       className="btn btn-default"
+                                                                                                       role="button">Button</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
